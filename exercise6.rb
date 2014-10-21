69ff84b94281024757080fde3bd168f1dd4f65e6
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-10-21 13:30:31 -0400

    Update2: Finished exercise6.rb

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,11 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"].sort
+grocery_list << "rice"
+grocery_list.delete("salmon")
+
+puts "Number of items: " + grocery_list.length.to_s
+puts "Second item in list: " + grocery_list[1]
 
 grocery_list.each { |item|
 	puts "* #{item}"
 }
+

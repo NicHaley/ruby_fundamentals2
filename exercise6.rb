2404b07b9a78ba888bc75cddc84d1dcff39a356f
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-10-21 14:49:13 -0400

    Step 4 and code clean up

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,23 @@
+#Step 1
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-grocery_list << "rice"
-puts "Total number of items: #{grocery_list.length}"
 
 def list(array)
 	array.each { |x|
 		puts "* #{x}"
 	}
+end
+
+#Step 2
+grocery_list << "rice"
+
+#Step 3
+puts "Total number of items: #{grocery_list.length}"
+
+#Step 4
+if grocery_list.include?("bananas")
+	puts "You need to pick up bananas"
+else
+	puts "You don't need to pick up bananas today"
 end
 
 list(grocery_list)

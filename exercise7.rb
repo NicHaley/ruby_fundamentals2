c9e5c65a7eea2c398929ab778a4780dd7951b4b1
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-10-21 15:29:18 -0400

    Finished exercise7.rb

--- exercise7.rb
+++ exercise7.rb
@@ -4,14 +4,29 @@ students = {
   :cohort3 => 22
 }
 
-#Step 1
+#Step 1 w/Step 4 Update
+
 def list(hash)
-	hash.each { |key, value|
-		puts "#{key}: #{value}"
-	}
+	hash.each do |key,value|
+		puts "#{key}: #{(value*1.05).to_i}"
+	end
 end
 
 #Step 2
 students[:cohort4] = 43
+
+#Step 3
+puts students.keys
+
+#Step 5
+students.delete(:cohort2)
+
+#BONUS
+counter = 0
+
+students.each do |key,value|
+	counter += (value*1.05).to_i
+	puts "Total students after #{key}: " + counter.to_s
+end
 
 list(students)

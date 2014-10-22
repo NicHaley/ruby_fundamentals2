bb127290b0571842b799dea8ac1f5fd1183b072c
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-10-22 11:23:47 -0400

    Made some fixes

--- exercise5.rb
+++ exercise5.rb
@@ -1,10 +1,10 @@
 def coversionToC (temp)
-	celcius = (temp.to_i - 32) * 5/9
-	puts "The temperature in C is #{celcius}"
+	return (temp.to_i - 32) * 5/9
 end
 
 puts "What is the temperature in F?"
 temp = gets.chomp
 
-coversionToC(temp)
+celcius = coversionToC(temp)
+puts "The temperature in C is #{celcius}"
 
